36e2d6cf2f5c1fecdab134f181832051abbe0998
Joji <freedive.cebu30@gmail.com>
2023-04-13 12:11:23 +0800

    Create question_19.go

---
+++ question_19.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	languages := []string{"ruby", "php", "python", "java"}
+	var languages2 []string
+	for _, language := range languages {
+		languages2 = append(languages2, strings.ToUpper(language))
+	}
+	fmt.Println(languages2)
+}
